9d8d1065439314fc927656656cb31ef336249cc3
dtodorovski24 <todorovskidavid3@gmail.com>
2022-08-05 10:14:46 +0200

    .

--- src/components/Movies/Movies.jsx
+++ src/components/Movies/Movies.jsx
@@ -1,10 +1,5 @@
-import React, { useState, useEffect } from "react";
-import {
-  Box,
-  CircularProgress,
-  useMediaQuery,
-  Typography,
-} from "@mui/material";
+import React, { useState } from "react";
+import { Box, CircularProgress, Typography } from "@mui/material";
 import { useSelector } from "react-redux";
 
 import { useGetMoviesQuery } from "../../services/TMDB";
